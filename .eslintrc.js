e32539998c73aa040b13e6bee14f91a06e8db25a
Romes <thonyromes@gmail.com>
2021-10-11 22:29:00 +0100

    chore: Setup lint staged and eslint unicorn

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,17 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:react/recommended",
+    "plugin:unicorn/recommended",
+    "airbnb",
+    "prettier",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
@@ -13,8 +23,70 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint", "prettier"],
+  plugins: [
+    "@typescript-eslint",
+    "react",
+    "react-hooks",
+    "prettier",
+    "import",
+    "unicorn",
+  ],
   rules: {
     "prettier/prettier": ["error"],
+    "react/jsx-sort-props": ["error", { ignoreCase: true }],
+    "unicorn/prevent-abbreviations": [
+      "error",
+      {
+        replacements: { args: false, props: false, ref: false, params: false },
+      },
+    ],
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/destructuring-assignment": ["error", "never"],
+    "eol-last": ["error", "always"],
+    "no-void": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "no-unused-vars": "warn",
+    "import/no-default-export": "error",
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".ts", ".tsx"],
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      plugins: ["@typescript-eslint"],
+      rules: {
+        "unicorn/require-post-message-target-origin": "off",
+        "no-use-before-define": "off",
+        "@typescript-eslint/no-use-before-define": ["error"],
+      },
+    },
+    {
+      files: "**/*.tsx",
+      rules: { "unicorn/no-null": "off", "prefer-destructuring": "off" },
+    },
+    {
+      files: "**/*.{test,spec}.{ts,tsx}",
+      rules: {
+        "import/no-extraneous-dependencies": [
+          "error",
+          { devDependencies: true },
+        ],
+        "react/jsx-props-no-spreading": "off",
+      },
+    },
+  ],
+  settings: {
+    react: {
+      createClass: "createReactClass",
+      pragma: "React",
+      fragment: "Fragment",
+      version: "detect",
+    },
   },
 };
