e4b0609035e9cdf168589fcf630aaf8aa91372f8
Romes <thonyromes@gmail.com>
2021-10-13 15:52:53 +0100

    feat: Create home view

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,7 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
 
-import { IncomingCall } from "@/components/calls";
+import { Home } from "@/views/home";
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
-    flex: 1,
-  },
-});
-
-const App = () => (
-  <View style={styles.container}>
-    <IncomingCall />
-  </View>
-);
+const App = () => <Home />;
 
 export { App };
